e134e8b5a01f09eb4ddb14348da2e54811d21083
lucas_jf12 <lucas_jf12@hotmail.com>
2020-02-05 22:11:26 -0300

    .

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,5 +1,5 @@
 class ClientsController < ApplicationController
-  #before_action :authenticate_user, only: [:index]
+  before_action :authenticate_user, only: [:index]
 
   # GET /clientes
   def index
